1ed5ab0028604a001ceac77ad2be2ac603a3375c
Justin Merz <jrmerz@gmail.com>
2020-09-01 15:51:34 -0700

    adding composite command

--- node-image-utils/index.js
+++ node-image-utils/index.js
@@ -2,14 +2,36 @@ const fs = require('fs-extra');
 const path = require('path');
 const CaReproject = require('./lib/ca-reproject');
 const jp2ToPngFn = require("./lib/jp2-to-png");
+const Composite = require('./lib/composite');
 
 if( process.argv[2] === 'jp2-to-png' ) {
   jp2ToPng();
-} else if( process.argv[2] === 'reproject' ) {
+} else if( process.argv[2] === 'composite' ) {
+  composite();
+} else if( process.agv[2] === 'reproject' ) {
   reproject();
 } else {
   console.error('Unknown command: '+process.argv[2]);
   process.exit(-1);
+}
+
+async function composite() {
+  let rootDir = process.argv.length > 2 ? process.argv[3] : process.cwd();
+  let images = await Composite.run(rootDir);
+  if( !images ) {
+    console.warn('Failed to create composite image');
+    return;
+  }
+
+  if( rootDir.match(/\/cells/) ) {
+    rootDir = path.resolve(rootDir, '..');
+  }
+
+  console.log('Writing composite file: '+path.join(rootDir, 'image.png'));
+  await fs.writeFile(path.join(rootDir, 'image.png'), images.sciPng);
+
+  console.log('Writing composite file: '+path.join(rootDir, 'web.png'));
+  await fs.writeFile(path.join(rootDir, 'web.png'), images.webPng);
 }
 
 async function reproject() {
---
+++ node-image-utils/lib/composite.js
@@ -0,0 +1,198 @@
+const fs = require('fs-extra');
+const path = require('path');
+const {PNG} = require('@ucd-lib/pngjs');
+const config = require('./config');
+
+class GRBCellComposite {
+
+  async run(dir) {
+    if( !dir.match(/\/cells/) ) {
+      dir = path.join(dir, 'cells');
+    }
+
+    let info = await this.getCompositeInfo(dir);
+    if( !info ) return null;
+
+    let sciPngData = new Uint16Array(info.width*info.height);
+    let webPngData = Buffer.alloc(info.width*info.height);
+
+    let histogram = {};
+    for( let cell of info.cells ) {
+      await this.addCell(dir, cell, info.width, sciPngData, webPngData, histogram);
+      // break;
+    }
+
+    let keys = Object.keys(histogram).map(i => parseInt(i));
+
+    keys.sort();
+    keys = keys.splice(1, keys.length-2);
+
+    let min = 99999999;
+    let max = 0;
+    for( let i = 0; i < keys.length; i++ ) {
+      if( min > keys[i] ) min = keys[i];
+      if( max < keys[i] ) max = keys[i];
+    }
+
+    let spread = max-min;
+    for( let i = 0; i < sciPngData.length; i++ ) {
+      webPngData[i] = (((sciPngData[i] - min) / spread) * 255);
+      if( webPngData[i] > 255 ) webPngData[i] = 255;
+      if( webPngData[i] < 0 ) webPngData[i] = 0;
+    }
+
+    let sciPng = new PNG({
+      colorType: 0,
+      inputColorType: 0,
+      bitDepth : 16,
+      height: info.height,
+      width: info.width
+    });
+    let webPng = new PNG({
+      colorType: 0,
+      inputColorType: 0,
+      height: info.height,
+      width: info.width
+    });
+
+    sciPng.data = sciPngData;
+    webPng.data = webPngData;
+
+    return {
+      sciPng : PNG.sync.write(sciPng, {
+        colorType: 0,
+        inputColorType: 0,
+        bitDepth : 16,
+        height: info.height,
+        width: info.width
+      }),
+      webPng : PNG.sync.write(webPng, {
+        colorType: 0,
+        inputColorType: 0,
+        height: info.height,
+        width: info.width
+      })
+    };
+  }
+
+  async addCell(dir, cell, compositeWidth, sciPngData, webPngData, histogram) {
+    if( !fs.existsSync(path.join(dir, cell, 'fragment-metadata.json')) ) {
+      console.warn('Failed to locate cell metadata: '+path.join(dir, cell, 'fragment-metadata.json'));
+      return;
+    }
+    let cellMetadata = await this.readMetadata(
+      path.join(dir, cell, 'fragment-metadata.json')
+    );
+    let imageMetadata = cellMetadata.fragment_headers_0.imagePayload;
+    
+    if( !fs.existsSync(path.join(dir, cell, 'image.png')) ) {
+      console.warn('Failed to locate cell data: '+path.join(dir, cell, 'image.png'));
+      return;
+    }
+    let imageData = await this.readPng(
+      path.join(dir, cell, 'image.png')
+    );
+
+    let row = imageMetadata.UPPER_LOWER_LEFT_Y_COORDINATE;
+    let col = imageMetadata.UPPER_LOWER_LEFT_X_COORDINATE;
+    let rval, val;
+
+    let productDef = config.apidProducts[cellMetadata.apid] || {};
+    let maxValue = productDef.maxValue || 65536;
+    let scale = productDef.scale || 1;
+    let spread = maxValue;
+    if( productDef.offsetBounds ) {
+      spread = productDef.offsetBounds.max - productDef.offsetBounds.min;
+    }
+
+    let offset = 0, loc;
+
+    // PNGJS always returns RGBA array, even if source was grayscale
+    for( let j = 0; j < imageData.data.length; j += 4 ) {
+
+      rval = imageData.data[j];
+      // val = Math.round(((rval*scale) / spread) * 255);
+
+      // if( val > 255 ) val = 255;
+      // else if( val < 0 ) val = 0;
+
+      if( !histogram[rval] ) histogram[rval] = 0;
+      histogram[rval]++;
+
+      if( offset !== 0 && offset % imageMetadata.IMAGE_BLOCK_WIDTH === 0 ) {
+        row++;
+      }
+
+      loc = (row * (compositeWidth)) + // current row (y) offset 
+       col + // column (x) offset of block
+       (offset % imageMetadata.IMAGE_BLOCK_WIDTH ); // column (x) offset within block
+
+      webPngData[loc] = 0;
+      sciPngData[loc] = rval;
+
+      offset++;
+    }
+  }
+
+  async readMetadata(file) {
+    return JSON.parse(await fs.readFile(file, 'utf-8'));
+  }
+
+  async readPng(file, opts) {
+    // assume 16bit grayscale
+    if( !opts ) {
+      opts = {
+        colorType: 0,
+        inputColorType: 0,
+        skipRescale : true,
+        bitDepth : 16
+      }
+    }
+
+    let imageData = await fs.readFile(file);
+
+    // return sync.read(imageData, opts);
+    return new Promise((resolve, reject) => {
+      new PNG(opts).parse(imageData, function (error, data) {
+        if( error ) reject(error);
+        else resolve(data);
+      });
+    });
+  }
+
+  async getCompositeInfo(dir) {
+    let max = {x: 0, y: 0};
+    let cells = await fs.readdir(dir);
+    
+    if( !cells.length ) {
+      console.warn('No cells found in directory: '+dir);
+      return
+    }
+
+    for( let cell of cells ) {
+      let [x, y] = cell.split('-').map(coord => parseInt(coord));
+      
+      if( x > max.x ) max.x = x;
+      if( y > max.y ) max.y = y;
+    }
+
+    if( !fs.existsSync(path.join(dir, cells[0], 'fragment-metadata.json')) ) {
+      console.warn('Failed to locate init cell metadata: '+path.join(dir, cells[0], 'fragment-metadata.json'));
+      return null;
+    }
+
+    let maxMetadata = await this.readMetadata(
+      path.join(dir, cells[0], 'fragment-metadata.json')
+    );
+    let imageMetadata = maxMetadata.fragment_headers_0.imagePayload;
+    
+    return {
+      cells,
+      width: max.x + imageMetadata.IMAGE_BLOCK_WIDTH,
+      height: max.y + imageMetadata.IMAGE_BLOCK_HEIGHT
+    }
+  }
+
+}
+
+module.exports = new GRBCellComposite();
