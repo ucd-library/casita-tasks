8ee98aac656d13d577b639a7638a6ffe66ef8afb
Justin Merz <jrmerz@gmail.com>
2020-11-13 09:41:33 -0800

    quick fix

--- decoder/run.sh
+++ decoder/run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 ssh-keyscan grb-box.cstars.ucdavis.edu >> /root/.ssh/known_hosts
-ssh ${SSH_KEY_USERNAME}@grb-box.cstars.ucdavis.edu "tail -f /grb/${GRB_FILE}/grbpackets.dat" | node node.js
+ssh ${SSH_KEY_USERNAME}@grb-box.cstars.ucdavis.edu "tail -f /grb/${GRB_FILE}/grbpackets.dat" | node index.js
