094ae2da86a496984efbcc5f900f80db7f38ed68
Justin Merz <jrmerz@gmail.com>
2020-11-13 09:20:14 -0800

    tweaks for decoder process

---
+++ decoder/default-kafka-setup.js
@@ -0,0 +1,41 @@
+const {logger} = require('@ucd-lib/krm-node-utils');
+
+let kafkaHost = 'kafka:9092';
+if( process.env.DECODER_KAFKA_HOST && process.env.DECODER_KAFKA_PORT ) {
+  kafkaHost = process.env.DECODER_KAFKA_HOST + ':' + process.env.DECODER_KAFKA_PORT;
+}
+
+module.exports = {
+  client : {
+    'metadata.broker.list' : kafkaHost,
+    'message.max.bytes': 25000000+'', // must be a string
+    'request.required.acks' : 1,
+    'dr_cb': true, // delivery report
+    'event_cb' : true
+    // 'statistics.interval.ms' : 500 // for event.stats callback
+  },
+  topic : {
+    topic : process.env.DECODER_KAFKA_TOPIC || 'goes-r-stream',
+    num_partitions: 10,
+    replication_factor: 1,
+    // TODO: this is set in decoder-krm-interface/index.js as well.  need to update both. badness
+    config : {
+      'retention.ms' : (1000 * 60 * 60)+'',
+      // 'message.max.bytes': 25000000+''
+      // 'max.message.bytes' : 100000000+''
+    }
+  },
+  callbacks : {
+    'ready' : () => logger.info(`${process.env.GRB_FILE} kafka producer ready`),
+    'disconnected' : e => logger.warn(`${process.env.GRB_FILE} kafka producer disconnected`, e),
+    'delivery-report' : (err, report) => {
+        if( err ) logger.error(`${process.env.GRB_FILE} kafka delivery report error`, err, report);
+    },
+    'event' : e => logger.info(`${process.env.GRB_FILE} kafka event`, e),
+    'event.error' : e => logger.error(`${process.env.GRB_FILE} kafka event.error`, e),
+    'event.stats' : e => logger.info('Kafka producer event.stats', e),
+    'produce.error' : (e, msg) => {
+        logger.error(`${process.env.GRB_FILE} local produce error`, e, msg.metadata)
+    }
+  }
+}
--- decoder/test.js
+++ decoder/test.js
@@ -1,32 +1,23 @@
 const path = require('path');
 const fs = require('fs');
+const {logger} = require('@ucd-lib/krm-node-utils');
 const Processor = require('@ucd-lib/goes-r-packet-decoder/lib/binary-stream-processor');
-
-let serverUrl = 'kafka:9092';
-if( process.env.DECODER_KAFKA_HOST && process.env.DECODER_KAFKA_PORT ) {
-  serverUrl = process.env.DECODER_KAFKA_HOST + ':' + process.env.DECODER_KAFKA_PORT;
-}
+const kafkaSetup = require('./default-kafka-setup');
 
 let processor = new Processor({
   live: true,
+  name : process.env.GRB_FILE,
   consoleLogStatus : false,
-  kafka : {
-    server : {
-      'metadata.broker.list' : serverUrl
-    },
-    topic : {
-      topic : process.env.DECODER_KAFKA_TOPIC || 'goes-r-stream',
-      num_partitions: 10,
-      options : {
-        'retention.ms' : 1000 * 60 * 15
-      }
-    }
-  }
+  filter : /^b1$/i,
+  onStreamClosed : () => logger.warn(`${process.env.GRB_FILE} grb tail stream closed`),
+  kafka : kafkaSetup
 });
 
-(async function() {
-  await processor.kafkaConnecting;
-  processor.pipe(fs.createReadStream(path.join(__dirname, 'testsecdecorded.dat')));
-  // setTimeout(() => process.exit(), 4000);
-})()
+processor.pipe();
+
+// (async function() {
+//   await processor.kafkaConnecting;
+//   processor.pipe(fs.createReadStream(path.join(__dirname, 'testsecdecorded.dat')));
+//   // setTimeout(() => process.exit(), 4000);
+// })()
 
