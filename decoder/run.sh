90f9e73680fa196bc02c6c2af076a09ab19feb1b
Justin Merz <jrmerz@gmail.com>
2021-01-13 10:10:26 -0800

    adding debug to ssh key startup

--- decoder/run.sh
+++ decoder/run.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 if [[ -f "/root/.ssh/id_rsa" ]]; then
+  echo "Updating /root/.ssh/id_rsa permissions"
   chmod 600 /root/.ssh/id_rsa 
 fi
+ls -al /root/.ssh
 ssh-keyscan grb-box.cstars.ucdavis.edu >> /root/.ssh/known_hosts
 ssh ${SSH_KEY_USERNAME}@grb-box.cstars.ucdavis.edu "tail -f /grb/${GRB_FILE}/grbpackets.dat" | node index.js
