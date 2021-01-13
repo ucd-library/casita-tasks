f6e106129f141e8fca65d7c89477795d58d651df
Justin Merz <jrmerz@gmail.com>
2021-01-13 10:01:38 -0800

    setting proper permissions of rsa key

--- decoder/run.sh
+++ decoder/run.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+if [[ -f "/root/.ssh/id_rsa" ]]; then
+  chmod 600 /root/.ssh/id_rsa 
+fi
 ssh-keyscan grb-box.cstars.ucdavis.edu >> /root/.ssh/known_hosts
 ssh ${SSH_KEY_USERNAME}@grb-box.cstars.ucdavis.edu "tail -f /grb/${GRB_FILE}/grbpackets.dat" | node index.js
