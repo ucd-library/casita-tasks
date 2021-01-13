c6080053002f521fc9dff916aede61b3aa426fd5
Justin Merz <jrmerz@gmail.com>
2021-01-13 10:27:14 -0800

    update

--- decoder/run.sh
+++ decoder/run.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-if [[ -f "/root/.ssh/id_rsa" ]]; then
+if [[ -e "/root/.ssh/id_rsa" ]]; then
   echo "Updating /root/.ssh/id_rsa permissions"
   chmod 600 /root/.ssh/id_rsa 
 fi
+
 ls -al /root/.ssh
+ls -al /root/.ssh/id_rsa
 ssh-keyscan grb-box.cstars.ucdavis.edu >> /root/.ssh/known_hosts
 ssh ${SSH_KEY_USERNAME}@grb-box.cstars.ucdavis.edu "tail -f /grb/${GRB_FILE}/grbpackets.dat" | node index.js
