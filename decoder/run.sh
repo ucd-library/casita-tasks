8530bb69aeec406e41c30eb32958609aeba9521b
Justin Merz <jrmerz@gmail.com>
2022-05-06 18:02:22 -0700

    switching to less

--- decoder/run.sh
+++ decoder/run.sh
@@ -12,4 +12,5 @@ if [[ -e "/root/.ssh/id_rsa" ]]; then
 fi
 
 ssh-keyscan grb-box.cstars.ucdavis.edu >> /root/.ssh/known_hosts
-ssh ${SSH_KEY_USERNAME}@grb-box.cstars.ucdavis.edu "tail -F /grb/${GRB_FILE}/grbpackets.dat" | node index.js
+# ssh ${SSH_KEY_USERNAME}@grb-box.cstars.ucdavis.edu "tail -F /grb/${GRB_FILE}/grbpackets.dat" | node index.js
+ssh ${SSH_KEY_USERNAME}@grb-box.cstars.ucdavis.edu "less +F -f /grb/${GRB_FILE}/grbpackets.dat" | node index.js
