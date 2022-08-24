9c2f620ba4b234efcf8dae02dd7786366632a301
Justin Merz <jrmerz@gmail.com>
2022-08-24 13:27:53 -0700

    wiring /thermal-anomaly endpoint

--- services/nodejs/rest/controllers/index.js
+++ services/nodejs/rest/controllers/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import status from './status.js';
 import webPush from './web-push.js';
+import thermalAnomaly from './thermal-anomaly.js';
 const router = express.Router();
 
 router.use('/status', status);
 router.use('/web-push', webPush);
+router.use('/thermal-anomaly', thermalAnomaly);
 
 export default router;
